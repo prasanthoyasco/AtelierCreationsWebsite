f21b3dc6bd21d0741fa957fafd247f6b3247206d
JagadishAtelier <jagadish.atelier@gmail.com>
2025-12-30 15:32:24 +0530

    updated mobile view

--- pages/BusinessSoftware.jsx
+++ pages/BusinessSoftware.jsx
@@ -59,7 +59,7 @@ const BusinessSoftware = () => {
             <div className="our-container-1">
               <div className="row justify-content-center">
                 {/* Service Card 1 */}
-                <div className="col-md-4 ">
+                <div className="col-md-4 mobile-development-page-grid">
                   <div className="service-card " data-aos="fade-up" data-aos-duration="1000">
                     <div className="srv-card-icon">
                       <img src={ERPSoftware} alt="" height="" width="" loading="lazy" title="" />
@@ -76,7 +76,7 @@ const BusinessSoftware = () => {
                   </div>
                 </div>
                 
-                <div className="col-md-4 " >
+                <div className="col-md-4 mobile-development-page-grid" >
                   <div className="service-card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
                     <div className="srv-card-icon">
                       <img src={HRMSSoftware} alt="" height="" width="" loading="lazy" title="" />
@@ -93,7 +93,7 @@ const BusinessSoftware = () => {
                   </div>
                 </div>
 
-                <div className="col-md-4 " >
+                <div className="col-md-4 mobile-development-page-grid" >
                   <div className="service-card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
                     <div className="srv-card-icon">
                       <img src={CRMSoftware} alt="" height="" width="" loading="lazy" title="" />
